22b5f2c4bf9a5348f98e889b3b0da27c9368bd5e
Thiago Tavares <tvttavares@gmail.com>
2022-02-13 18:12:15 -0400

    Adding swagger

---
+++ src/main/java/com/tvttavares/neo4jdataextractor/config/SwaggerLinks.java
@@ -0,0 +1,9 @@
+package com.tvttavares.neo4jdataextractor.config;
+
+public class SwaggerLinks {
+
+    public static final String USER = "User";
+    public static final String ALL_USERS = "Return all Users in Json format";
+    public static final String USER_PAYMENT_CSV = "Return Users and paymentId's in a CSV format";
+
+}
