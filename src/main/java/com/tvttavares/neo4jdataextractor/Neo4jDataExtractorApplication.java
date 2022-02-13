06a5bd89ade7f883952a1b78c2bbe90b0e9f47f9
Thiago Tavares <tvttavares@gmail.com>
2022-02-13 18:06:08 -0400

    Initial project

---
+++ src/main/java/com/tvttavares/neo4jdataextractor/Neo4jDataExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.tvttavares.neo4jdataextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jDataExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jDataExtractorApplication.class, args);
+    }
+
+}
